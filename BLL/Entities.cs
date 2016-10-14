ad73f6dfd342823b948f392db0ee8954d584d8cd
ldavtyan <lilit.davtyan@monitis.com>
2016-10-14 10:57:56 +0400

    Create solution with projects

---
+++ BLL/Entities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class Entities
+    {
+        public string teamid { get; set; }
+        public string teamname {get; set; }
+        public string memberid { get; set; }
+        public string membername { get; set; }
+        public string membersurname { get; set; }
+        public string projectid { get; set; }
+        public string projectduedate { get; set; }
+        public string projectdescription { get; set; }
+    }
+}
