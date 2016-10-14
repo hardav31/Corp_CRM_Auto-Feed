1a22b0950dc30bccebe841ebb290b96afa76a27c
ldavtyan <lilit.davtyan@monitis.com>
2016-10-14 12:07:41 +0400

    Adding IConvertor Interface and creating CSV and XML convertors inheryted from IConverto interface

---
+++ BLL/Interfaces/IConvertor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    interface IConvertor
+    {
+
+        string Serialize<T>(T item);
+        T DeSerialize<T>(string data);
+
+    }
+}
