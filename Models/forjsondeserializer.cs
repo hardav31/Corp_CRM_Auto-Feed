77cdc47580841a94a373f877add9b26547f29ac5
ldavtyan <lilit.davtyan@monitis.com>
2016-11-17 17:20:37 +0400

    merge

---
+++ Models/forjsondeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    //FOR TESTING JSON SERIALIZER?DESERIALIZER
+    public class forjsondeserializer
+    {
+        public List<Team> team { get; set; }
+        public List<Project> project { get; set; }
+        public forjsondeserializer(List<Team> t, List<Project> p)
+        {
+            team = t;
+            project = p;
+        }
+    }
+}
