cbdd4ce2a00dfbb01adcb6380f96a77417921499
Galust <bayburcyan.galust@gmail.com>
2016-11-18 23:41:50 +0400

    finaly

--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -11,17 +11,10 @@ namespace FileManager
         static void Main(string[] args)
         {
             AppConfigManager.appSettings.AppReader();
-            //LoggerType.CreateLogger(AppConfigManager.appSettings.LogToConsole, AppConfigManager.appSettings.LogToEventLog, AppConfigManager.appSettings.LogToFile);
-            LoggerType.CreateLogger(AppConfigManager.appSettings);
+            LoggerType.CreateLogger(AppConfigManager.appSettings.LogToConsole, AppConfigManager.appSettings.LogToEventLog, AppConfigManager.appSettings.LogToFile);
             FolderMonitor check = new FolderMonitor(AppConfigManager.appSettings.FolderMonitorPath);
             DirectoryReader.directoryReader.ReadAllFiles(AppConfigManager.appSettings.FolderMonitorPath);
 
-
-            //FOR TESTING JSON SERIALIZER/DESERIALIZER
-            //JsonParser jp = new JsonParser();
-            //jp.JsonDeserializ();
-
-            // DirectoryReader.directoryReader.ReadAllFiles(AppConfigManager.appSettings.FolderMonitorPath);
             Console.ReadKey();
         }
 
